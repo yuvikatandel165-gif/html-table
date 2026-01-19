eccb27c643fe5f7f236ab63df06d050dab979b5c
yuvikatandel165-gif <yuvikatandel165@gmail.com>
2026-01-19 15:11:34 +0530

    Create index.html.for

---
+++ index.html.for
@@ -0,0 +1,37 @@
+<html>
+  <head>
+    <style>
+      table,th,td
+      {border : 1px solid red ; padding:5px ; text allign : left ;}
+    </style>
+  </head>
+  <body>
+    <h2> basic table <h2>
+      <p> To add a caption to a table, use the caption tag</p>
+      <table>
+        <caption> Employee Details </caption>
+        <tr>
+          <td> name </td>
+          <td> age </td>
+          <td> adress </td>
+        </tr>
+        <tr>
+          <td> soham </td>
+          <td> 20 </td>
+          <td> mumbai </td>
+        </tr>
+        <tr>
+          <td> mohan </td>
+          <td> 19 </td>
+          <td> dubai </td>
+        </tr>
+      </table>
+    </h2>
+    </h2>
+  </body>
+</html>
+
+          
+          
+        
+   
